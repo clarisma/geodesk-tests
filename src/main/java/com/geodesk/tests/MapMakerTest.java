07359dc005b9645341ab79a94ab30a7b516b9493
clarisma <56932851+clarisma@users.noreply.github.com>
2023-12-10 17:58:03 +0100

    Release 0.1.9

---
+++ src/main/java/com/geodesk/tests/MapMakerTest.java
@@ -0,0 +1,38 @@
+package com.geodesk.tests;
+
+import com.geodesk.feature.Feature;
+import com.geodesk.feature.FeatureLibrary;
+import com.geodesk.util.MapMaker;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static com.geodesk.feature.Filters.*;
+
+public class MapMakerTest
+{
+    FeatureLibrary features;
+
+    @Before public void setUp()
+    {
+        features = new FeatureLibrary("c:\\geodesk\\tests\\world.gol");
+    }
+
+    @After public void tearDown()
+    {
+        features.close();
+    }
+
+    @Test public void testFrance() throws IOException
+    {
+        Feature france = features.select("a[boundary=administrative][admin_level=2][name=France]").first();
+        MapMaker map = new MapMaker();
+
+        map.add(features
+            .select("a[boundary=administrative][admin_level=6]")
+            .select(within(france)));
+        map.save(TestSettings.outputPath().resolve("france.html").toString());
+    }
+}
